a8b00741425158639a08b3cdc0e1faf6a786b1c6
angrypotaoe19 <71114489+adityaraj20@users.noreply.github.com>
2021-04-24 02:34:50 -0700

    Create kangaroo.cpp

---
+++ kangaroo.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int x1,v1,x2,v2;
+    cin >> x1 >> v1 >>x2 >> v2;
+    
+    if (v1>v2){
+        while(x1<=x2){
+            if(x1==x2){
+                cout<<"YES";
+                return 0;
+            }
+            x1=x1+v1;
+            x2=x2+v2;
+            
+        }
+    }
+    if(v1==v2){
+        if(x1==x2){
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+    return 0;
+}
