13873efbadb4744ddbab1ab541fa35ad310bcc4c
angrypotaoe19 <71114489+adityaraj20@users.noreply.github.com>
2021-04-29 19:30:37 +0530

    Create migratory_birds.cpp

---
+++ migratory_birds.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    
+    int n , t , maxi=0;
+    cin >> n;
+    int types[5]={};
+    while(n--){
+        cin >> t;
+        types[t-1]++;
+    }
+    for(int i=1 ; i<5 ; i++){
+        if(types[i]>types[maxi]) maxi=i;
+    }
+    cout << maxi+1 << endl;
+    return 0;
+}
