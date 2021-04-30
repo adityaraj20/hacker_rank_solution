466ca14e8bcdf1475e34fcdde184267ae77b6910
angrypotaoe19 <71114489+adityaraj20@users.noreply.github.com>
2021-04-30 10:22:55 +0530

    Create electronic-shop.cpp

---
+++ electronic-shop.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n, m , s;
+    cin >> s >> n >> m;
+    
+    vector<int> keyboard(n);
+    vector<int> usb(m);
+    for(int i=0; i<n ; i++)cin >> keyboard[i];
+    for(int i=0; i<m ; i++)cin >> usb[i];
+    sort(usb.begin(), usb.end());
+    sort(keyboard.begin(), keyboard.end());
+    
+    if(keyboard[0]+usb[0]>s)cout << -1 << endl;
+    else
+    {
+        int  mini = s-keyboard[0] - usb[0];
+        for (int i=0; i < n; i++)
+        {
+            if(keyboard[i]+usb[0]>s)break;
+            for(int j=0; j<m; j++){
+                if(keyboard[i]+usb[j]>s) break;
+                else mini = min(s-keyboard[i]-usb[j],mini);
+                
+            }
+        }
+        cout << s-mini << endl;
+    }
+    return 0;
+}
